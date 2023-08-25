1c9585d2099945e14dedebc6981ddbce65b18e1a
ssparknt <sina.hakim.red@gmail.com>
2023-08-25 14:01:26 +0300

    metatags are added!

--- src/App.tsx
+++ src/App.tsx
@@ -110,7 +110,8 @@ function App() {
         src="/images/bgImage.webp"
         alt="dashed SF-Symbols logo as the background image"
       />
-      <div className="bg-gradient-to-t from-emerald-100 via-emerald-100 to-white">
+
+      <section className="bg-gradient-to-t from-emerald-100 via-emerald-100 to-white">
         <header className="flex items-center relative justify-between mx-5 md:mx-20 lg:mx-40 my-10">
           <div className="flex items-center gap-3">
             <img
@@ -120,50 +121,70 @@ function App() {
             />
             <h1 className="text-2xl lg:text-3xl font-bold">SF-Symbols</h1>
           </div>
-          <a
-            href="https://github.com/sina-sparkn/SF-Symbols-Icons"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center gap-2 text-xs md:text-base hover:bg-emerald-200/80 duration-200 py-1.5 px-3 pr-3 pl-2.5 rounded-full"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 496 512"
-              className="w-5 h-5"
-            >
-              <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
-            </svg>
-            Give it a Star
-          </a>
         </header>
 
-        <div className="mb-36 mx-5 md:mx-20 lg:mx-40 flex flex-wrap gap-1.5 text-zinc-600 text-sm relative">
-          <p>5000+ Symbols</p> •
-          <div>
+        <div className="mb-32 mx-5 md:mx-20 lg:mx-40 flex flex-col gap-1.5 text-zinc-600 text-sm relative">
+          <div className="flex flex-wrap gap-2">
+            <p>5000+ Symbols</p> •
+            <div>
+              <a
+                className="underline underline-offset-2 hover:no-underline"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://creativecommons.org/licenses/by/4.0/"
+              >
+                CC BY 4.0
+              </a>
+              <span className="ml-1">License</span>
+            </div>
+            •
+            <div>
+              Symbols by{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline underline-offset-2 hover:no-underline"
+                href="https://uladluch.notion.site"
+              >
+                Ulad Luch
+              </a>
+            </div>
+            • <p>React Compatible</p>
+          </div>
+          <div className="flex flex-col sm:flex-row gap-2 sm:items-center mt-5">
             <a
-              className="underline underline-offset-2 hover:no-underline"
               target="_blank"
               rel="noopener noreferrer"
-              href="https://creativecommons.org/licenses/by/4.0/"
+              href="https://www.figma.com/community/file/886999666531731323"
+              className="hover:bg-emerald-300 duration-200 flex w-fit items-center gap-2 border border-zinc-300 rounded-lg px-5 py-2 text-zinc-900 "
             >
-              CC BY 4.0
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 384 512"
+                className="w-4 h-4"
+              >
+                <path d="M14 95.7924C14 42.8877 56.8878 0 109.793 0H274.161C327.066 0 369.954 42.8877 369.954 95.7924C369.954 129.292 352.758 158.776 326.711 175.897C352.758 193.019 369.954 222.502 369.954 256.002C369.954 308.907 327.066 351.795 274.161 351.795H272.081C247.279 351.795 224.678 342.369 207.666 326.904V415.167C207.666 468.777 163.657 512 110.309 512C57.5361 512 14 469.243 14 416.207C14 382.709 31.1945 353.227 57.2392 336.105C31.1945 318.983 14 289.5 14 256.002C14 222.502 31.196 193.019 57.2425 175.897C31.196 158.776 14 129.292 14 95.7924ZM176.288 191.587H109.793C74.2172 191.587 45.3778 220.427 45.3778 256.002C45.3778 291.44 73.9948 320.194 109.381 320.416C109.518 320.415 109.655 320.415 109.793 320.415H176.288V191.587ZM207.666 256.002C207.666 291.577 236.505 320.417 272.081 320.417H274.161C309.737 320.417 338.576 291.577 338.576 256.002C338.576 220.427 309.737 191.587 274.161 191.587H272.081C236.505 191.587 207.666 220.427 207.666 256.002ZM109.793 351.795C109.655 351.795 109.518 351.794 109.381 351.794C73.9948 352.015 45.3778 380.769 45.3778 416.207C45.3778 451.652 74.6025 480.622 110.309 480.622C146.591 480.622 176.288 451.186 176.288 415.167V351.795H109.793ZM109.793 31.3778C74.2172 31.3778 45.3778 60.2173 45.3778 95.7924C45.3778 131.368 74.2172 160.207 109.793 160.207H176.288V31.3778H109.793ZM207.666 160.207H274.161C309.737 160.207 338.576 131.368 338.576 95.7924C338.576 60.2173 309.737 31.3778 274.161 31.3778H207.666V160.207Z" />
+              </svg>
+              Get figma file
             </a>
-            <span className="ml-1">License</span>
-          </div>
-          •
-          <div>
-            Symbols by{" "}
             <a
+              href="https://github.com/sina-sparkn/SF-Symbols-Icons"
               target="_blank"
               rel="noopener noreferrer"
-              className="underline underline-offset-2 hover:no-underline"
-              href="https://uladluch.notion.site"
+              className="hover:bg-emerald-300 duration-200 flex w-fit items-center gap-2 border border-zinc-300 rounded-lg px-5 py-2 text-zinc-900 "
             >
-              Ulad Luch
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 496 512"
+                className="w-5 h-5"
+              >
+                <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+              </svg>
+              Give it a Star
             </a>
           </div>
-          • <p>React Compatible</p>
         </div>
 
         <section className="flex flex-col gap-3 mx-5 md:mx-20 lg:mx-40 relative">
@@ -184,9 +205,9 @@ function App() {
               />
             </svg>
           </div>
+
           <div className="relative">
             <div className="absolute w-10 h-10 bg-gradient-to-l from-emerald-100 to-transparent right-0 top-0" />
-
             <ul className="grid grid-flow-col items-center gap-3 w-full pb-4 overflow-x-scroll">
               {Categories.map((item) => (
                 <li
@@ -206,7 +227,7 @@ function App() {
             </ul>
           </div>
         </section>
-      </div>
+      </section>
 
       <section className="searchF z-50 w-full sticky h-full -top-1">
         <section className="w-full h-full p-5 flex flex-col lg:flex-row lg:items-center text-zinc-600 mb-3 shadow bg-white md:px-20 lg:px-40">
@@ -313,7 +334,7 @@ function App() {
       </section>
 
       <section className="mx-5 md:mx-20 lg:mx-40 mt-7 mb-10">
-        <section className="grid grid-cols-2 sm:grid-cols-3 w-full md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 justify-items-center content-center place-items-center place-content-center gap-y-7 ">
+        <div className="grid grid-cols-2 sm:grid-cols-3 w-full md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 justify-items-center content-center place-items-center place-content-center gap-y-7 ">
           {filteredData?.length === 0 && (
             <div className="w-full text-zinc-500  flex items-center justify-center mt-5 mb-2 absolute">
               No SF-Symbols for "{searchQuery}"
@@ -358,8 +379,9 @@ function App() {
               )}
             </div>
           ))}
-        </section>
+        </div>
       </section>
+
       <hr />
       <footer className="mt-10 mb-20 mx-5 md:mx-40 flex justify-between text-sm">
         <div className="flex items-center gap-2">
@@ -396,6 +418,7 @@ function App() {
           Give it a Star
         </a>
       </footer>
+
       <motion.button
         onClick={() => {
           window.scrollTo(0, 0);
